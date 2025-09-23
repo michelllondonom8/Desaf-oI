e168f8a32a87c7f7a2ea2e71c431d88b1e11bf71
michelllondonom8 <michell.londonom@udea.edu.co>
2025-09-22 21:53:28 -0500

    Modificacion de rotate_left_byte, creacion de rotate_right_byte y funcion de procesar_mensaje en Encriptacion

--- Desafio/encriptacion.cpp
+++ Desafio/encriptacion.cpp
@@ -4,17 +4,45 @@ Encriptacion::Encriptacion() {}
 
 uint8_t Encriptacion::rotate_left_byte(uint8_t b, unsigned int n)
 {
-    n &= 7u;
+    n = n % 8;
+    if (n != 0) {
+        uint16_t tmp = b;
+        return (uint8_t)(((tmp << n) | (tmp >> (8 - n))) & 0xFF);
+    }
+    return b;
+}
 
-    if (n == 0) return b;
+uint8_t Encriptacion::rotate_right_byte(uint8_t b, unsigned int n)
+{
+    n = n % 8;
+    if (n != 0) {
+        uint16_t tmp = b;
+        return (uint8_t)(((tmp >> n) | (tmp << (8 - n))) & 0xFF);
+    }
+    return b;
+}
 
+uint8_t Encriptacion::aplicar_clave(uint8_t b, uint8_t key)
+{
+    return b ^ key;
+}
 
+void Encriptacion::procesar_mensaje(uint8_t *mensaje, int longitud, uint8_t key, bool rotar_izq, unsigned int n)
+{
+    for (int i = 0; i < longitud; i= i+1) {
+        uint8_t b = mensaje[i]; // aqui agarro el byte actual
 
-    unsigned int tmp = (unsigned int)b;
+        // primero se rota, segun lo que digamos
+        if (rotar_izq) {
+            b = rotate_left_byte(b, n);
+        } else {
+            b = rotate_right_byte(b, n);
+        }
 
+        // despues de rotar se aplica la clave con XOR
+        b = aplicar_clave(b, key);
 
-    unsigned int res = ((tmp << n) | (tmp >> (8 - n))) & 0xFFu;
-
-
-    return (uint8_t)res;
+        // guardo el resultado otra vez en el mensaje
+        mensaje[i] = b;
+    }
 }
--- Desafio/encriptacion.h
+++ Desafio/encriptacion.h
@@ -7,6 +7,9 @@ class Encriptacion
 public:
     Encriptacion();
     uint8_t rotate_left_byte(uint8_t b, unsigned int n);
+    uint8_t rotate_right_byte(uint8_t b, unsigned int n);
+    uint8_t aplicar_clave(uint8_t b, uint8_t key);
+    void procesar_mensaje(uint8_t* mensaje, int longitud, uint8_t key, bool rotar_izq, unsigned int n);
 };
 
 #endif // ENCRIPTACION_H
